f7d9065e6ed72edd09d55db0a9d0a1ed3b8d9ca7
ShotaTogawa <s.togawa0112@gmail.com>
2020-02-08 16:29:34 -0800

    changed

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -15,23 +15,8 @@ module.exports = app => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redisURL = "redis://127.0.0.1:6379";
-    const client = redis.createClient(redisURL);
-    const util = require("util");
-    client.get = util.promisify(client.get);
-
-    // Do we have any cached data in redis related to this query
-    const cachedBlogs = await client.get(req.user.id);
-    // if yes, then respond to the request right away and return
-    if (cachedBlogs) {
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    // if no, we need to request and update our cache to store the data
     const blogs = await Blog.find({ _user: req.user.id });
-
     res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post("/api/blogs", requireLogin, async (req, res) => {
