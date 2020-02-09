9c5e1be2b814c6907f5f16a2bfec2a501884f7af
ShotaTogawa <s.togawa0112@gmail.com>
2020-02-08 16:17:15 -0800

    rewrote original function of mongoose

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const redis = require("redis");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,13 +14,27 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const redisURL = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisURL);
+    const util = require("util");
+    client.get = util.promisify(client.get);
+
+    // Do we have any cached data in redis related to this query
+    const cachedBlogs = await client.get(req.user.id);
+    // if yes, then respond to the request right away and return
+    if (cachedBlogs) {
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // if no, we need to request and update our cache to store the data
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
